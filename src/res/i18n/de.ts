8797f51d074616a4bbd75f208eee21f5cb69f3db
Muhammad Irfan Khan <irfank480@gmail.com>
2022-09-12 03:21:50 +0800

    Added Gernam Translation

--- src/res/i18n/de.ts
+++ src/res/i18n/de.ts
@@ -1,35 +1,33 @@
 export default {
-// Add info form headers
-  addInfoHeaderTitle: 'Fügen Sie Ihre Informationen hinzu',
-  addInfoHeaderDetailMessage:'Um Ihr Profil zu vervollständigen, füllen Sie bitte alle notwendigen Informationen aus',
-// Add info form placholders
-  firstNamePlaceHolder: "First name ge",
-  lastNamePlaceHolder: "Last name ge ",
-  agePlaceHolder: "age ge",
-  phoneNumberPlaceHolder: "Phone number ge",
-  email: "Email ge",
-  continue: "Continue ge",
-  logout: "Log out",
-  submitButtonText: "Submit ge",
-  startOver: "Start Over",
-  submissionResultSuccessText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
-// Assessment info
-  assessmentInfo: "(Localized/Multi-Lingual) Solution for Senior React Native Developer Assessment",
-
-  selectYourLanguage: "Select your Language",
-  enterYourEmailAddress: "Enter your email address",
-  demoAccount: 'Demo Account',
-  yourEmailAddress: "Your email address",
-  yourNewAccount: "Your new account",
-  dummyText: "Bacon ipsum dolor amet kielbasa filet mignon biltong hamburger tri-tip sirloin. Andouille burgdoggen filet mignon salami biltong chicken venison.",
-  whatCountryDoYouLiveIn: "What country do you live in?",
-  whichStateDoYouLiveIn: "Which state do you live in?",
-  selectCountry:"Select country",
-  country:"Country",
-  verifiedYourEmail: "Verified your email",
-  checkEmails: "Please check your emails",
-  cancel:"cancel",
-  emailVerfied: "Email verified!",
-  waitRedirect: "Please wait while we redirect you..."
+  // Add info form headers
+ addInfoHeaderTitle: 'Fügen Sie Ihre Informationen hinzu',
+ addInfoHeaderDetailMessage:'Um Ihr Profil zu vervollständigen, füllen Sie bitte alle notwendigen Informationen aus',
+ // Add info form placholders
+ firstNamePlaceHolder: "Vorname",
+ lastNamePlaceHolder: "Nachname",
+ agePlaceHolder: "das Alter",
+ phoneNumberPlaceHolder: "Telefonnummer",
+ email: "Email",
+ continue: "Fortsetzen",
+ submitButtonText: "Einreichen",
+ startOver: "Von vorn anfangen",
+ // Assessment info
+ logout: "Ausloggen",
+ assessmentInfo: "(Lokalisierte/mehrsprachige) Lösung für Senior React Native Developer Assessment",
+ selectYourLanguage: "Bitte wählen Sie Ihre Sprache",
+ submissionResultSuccessText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco",
+ enterYourEmailAddress: "Geben sie ihre E-Mailadresse ein",
+ demoAccount: 'Demo-Konto',
+ yourEmailAddress: "deine Emailadresse",
+ yourNewAccount: "Ihr neues Konto",
+ dummyText: "Bacon ipsum dolor amet Kielbasa Filet Mignon Biltong Hamburger Tri-Tip-Lendenstück. Andouille burgdoggen filet mignon salami biltong chicken hirsch.",
+ whatCountryDoYouLiveIn: "In welchem ​​Land lebst du?",
+ whichStateDoYouLiveIn: "In welchem ​​Bundesland lebst du?",
+ selectCountry:"Land auswählen",
+ country:"Land",
+ verifiedYourEmail: "Bestätigte Ihre E-Mail",
+ checkEmails: "Bitte überprüfen Sie Ihre E-Mails",
+ cancel:"Abbrechen",
+ emailVerfied: "Email überprüft!",
+ waitRedirect: "Bitte warten Sie, während wir Sie weiterleiten..."
 };
-
